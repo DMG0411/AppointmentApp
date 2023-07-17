92ff0fcf16f110dc0d866d0970c9fd973287bfec
DMG0411 <m2bobko@gmail.com>
2023-07-17 11:40:11 +0300

    Authenticate feature added

--- appointment_app/resources/views/welcome.blade.php
+++ appointment_app/resources/views/welcome.blade.php
@@ -16,20 +16,12 @@
         </style>
     </head>
     <body class="antialiased">
-        <strong>Database Connected: </strong>
-        <?php
-        try {
-            \DB::connection()->getPDO();
-            echo \DB::connection()->getDatabaseName();
-            } catch (\Exception $e) {
-            echo 'None';
-        }
-    ?>
+ 
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
-                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
@@ -57,10 +49,10 @@
                                     </svg>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Choose a date for an appointment</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                    Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                    An appointment last 1 hour
                                 </p>
                             </div>
 
